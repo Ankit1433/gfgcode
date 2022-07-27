58762dd9b303f848667fd824c947c5ecd4cd0f7e
ankit prajapati <91253833+Ankit1433@users.noreply.github.com>
2022-07-27 22:05:18 +0530

    check palindrome number

--- AppData/Roaming/JetBrains/IdeaIC2022.1/scratches/scratch_1.java
+++ AppData/Roaming/JetBrains/IdeaIC2022.1/scratches/scratch_1.java
@@ -1,22 +1,24 @@
-import java.util.*;
-//find number of digits in number.
+//check palindrome number.
 class scratch_1 {
-    static int recursive(int n){
-        if(n==0){
-            return 0;
+    static boolean ispalindrome(int n){
+        int a=0,b=n;
+        while (b!=0){
+           int  temp=b%10;
+            a=a*10+temp;
+            b=b/10;
         }
-        return 1+recursive(n/10);
+        return (a==n);
     }
+
     public static void main(String[] args) {
-        int count=0,n=123;
+        int n=121;
+    if(ispalindrome(n)==true){
+        System.out.println("yes it is a palindrome number.");
 
-        System.out.println(recursive(n));
-        while(n!=0) {
-        n=n/10;
-        count++;
+    }
+    else {
+        System.out.println("not a palindrome.");
+    }
+    }
+
         }
-        System.out.println(count);
-    }
-
-
-    }
