280bcd4706f614e21a9dc4809dc36abe4cc2ce20
ankit prajapati <91253833+Ankit1433@users.noreply.github.com>
2022-07-27 21:11:42 +0530

    find no of digits in number

--- AppData/Roaming/JetBrains/IdeaIC2022.1/scratches/scratch_1.java
+++ AppData/Roaming/JetBrains/IdeaIC2022.1/scratches/scratch_1.java
@@ -1,8 +1,16 @@
 import java.util.*;
 //find number of digits in number.
 class scratch_1 {
+    static int recursive(int n){
+        if(n==0){
+            return 0;
+        }
+        return 1+recursive(n/10);
+    }
     public static void main(String[] args) {
         int count=0,n=123;
+
+        System.out.println(recursive(n));
         while(n!=0) {
         n=n/10;
         count++;
