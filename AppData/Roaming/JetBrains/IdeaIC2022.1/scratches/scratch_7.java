bb5e304e3a715bbdc5c2b7eae0e3aee899df8fef
ankit prajapati <91253833+Ankit1433@users.noreply.github.com>
2022-08-31 22:46:57 +0530

    to find index of second largest array element by naive and efficient approach

--- AppData/Roaming/JetBrains/IdeaIC2022.1/scratches/scratch_7.java
+++ AppData/Roaming/JetBrains/IdeaIC2022.1/scratches/scratch_7.java
@@ -1,5 +1,5 @@
 class Scratch {
-    static int largest(int arr[]){
+   /* static int largest(int arr[]){
         int res=0;
             for(int i=1;i<arr.length;i++){
                 if(arr[i]>arr[res]) {
@@ -25,10 +25,28 @@ class Scratch {
             }
         }
         return res;
+    }*/
+
+
+    static int secondlargest1(int arr[]){
+        int res=-1,largest=0;
+        for(int i=1;i<arr.length;i++) {
+            if (arr[i] > arr[largest]) {
+                res = largest;
+                largest = i;
+            } else {
+                if (arr[i] != arr[largest]) {
+                    if (res == -1 || arr[i] > arr[res]) {
+                        res = i;
+                    }
+                }
+            }
+        }
+        return res;
     }
     public static void main(String[] args) {
-        int arr[]={1,6,4,2,87};
-        System.out.println(secondlargest(arr));
+        int arr[]={1,12,4,24,87};
+        System.out.println(secondlargest1(arr));
         
     }
 }
